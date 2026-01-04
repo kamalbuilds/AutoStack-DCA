34d42b65a31f25216963fadf184c1756888c64eb
kamalbuilds <geniusamansingh@gmail.com>
2026-01-04 17:43:34 +0530

    frontend setup

---
+++ frontend/src/lib/wagmi.ts
@@ -0,0 +1,19 @@
+import { http, createConfig } from 'wagmi'
+import { baseSepolia } from 'wagmi/chains'
+import { metaMask } from 'wagmi/connectors'
+
+export const config = createConfig({
+  chains: [baseSepolia],
+  connectors: [
+    metaMask(),
+  ],
+  transports: {
+    [baseSepolia.id]: http(),
+  },
+})
+
+declare module 'wagmi' {
+  interface Register {
+    config: typeof config
+  }
+}
